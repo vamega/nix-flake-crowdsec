0d17910a6b8ad38b7ddbd04dbeced73a0052d28b
Louis Roebben <louis.roebben@kuleuven.be>
2024-09-28 12:14:35 +0200

    Update crowdsec package to v1.6.3

--- packages/crowdsec/default.nix
+++ packages/crowdsec/default.nix
@@ -32,16 +32,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 }:
 buildGoModule rec {
   pname = "crowdsec";
-  version = "1.6.2";
+  version = "1.6.3";
 
   src = fetchFromGitHub {
     owner = "crowdsecurity";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-3GpSpADtCNvekR7gjbIlqzog7PMog6Sra5tpcnUf/gk=";
+    hash = "sha256-JN2siYUUFPSz+SyQwkX6DZ9k82SGHEQ1QHBEIfEV4EM=";
   };
 
-  vendorHash = "sha256-kkQYKiOSmFHjhOrYV40YRZX9w6rUue0NSKfw+Bqxb9s=";
+  vendorHash = "sha256-fl5LkRz69QOq4aPyAhMFxw1FWozLzofDBUGvRptuyZY=";
 
   nativeBuildInputs = [installShellFiles];
 
